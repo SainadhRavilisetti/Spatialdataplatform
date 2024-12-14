3e6f417363c55dfeddd8db9b4b79fbd6c5564b48
saina <saina@LAPTOP-LT20HSUT>
2024-12-14 13:08:17 +0530

    Add project files.

---
+++ Spatialdataplatform/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Spatialdataplatform.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
